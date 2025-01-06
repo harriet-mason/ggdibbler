944588473bbaccecd8a83e1cc2a34e917e55f6df
harriet-mason <harriet.mason@monash.edu>
2025-01-06 22:05:40 +1100

    got working VSUP (had to also fix shrinkage_mix)

--- R/VSUP.R
+++ R/VSUP.R
@@ -49,7 +49,7 @@ hex2hcl <- function(colours){
 
 
 # mixcolour function used in the ASRA paper
-  # the colourspace one throws a warning messes up if you have convex colours, something I fixed for the the ARSA function
+  # the colorspace one throws a warning messes up if you have convex colours, something I fixed for the the ARSA function
 ARSA_mix <- function(basecols, p_length, nblend) {
   X <- rgb2hsv(col2rgb(unique(basecols)))
   v1 <- X[,seq(1,dim(X)[2], 2)]
@@ -87,7 +87,7 @@ tree_mix <- function(colours, alpha=0.5){
     colours <- colorspace::hex2RGB(colours)
     for(i in seq(1, m, by=2)){
       colour <- colorspace::mixcolor(alpha, colours[i], colours[i+1])
-      pal <- c(pal, rep(colourspace::hex(colour), 2*n/m)) #keep output same length as input
+      pal <- c(pal, rep(colorspace::hex(colour), 2*n/m)) #keep output same length as input
       }
   # if odd # of colours
     } else {
@@ -96,21 +96,35 @@ tree_mix <- function(colours, alpha=0.5){
   pal
 }
 
-shrinkage_mix <- function(colours, colour_mean, alpha){
-  # alpha = b/a+b where b=est_var and a=global_var
-  # mixing is only for adjustment on mean, not on the variance adjustment
-  n <- length(colours)
+shrinkage_mix <- function(colours, colour_mean=NULL, alpha=0.5){
+  # get vector length
+  n = length(colours)
+  
   # convert to RGB for mixing
-  colour_mean <- colorspace::hex2RGB(colour_mean)
   colours <- colorspace::hex2RGB(colours) 
+  
+  # If mean colour not supplied, take middle value
+  if (is.null(colour_mean)) {
+    if(n%%2==0){
+      colour_mean <- colorspace::mixcolor(0.5, colours[n/2], colours[(n/2)+1])
+    } else {
+      med <- (n+1)/2 
+      colour_mean <- colorspace::hex2RGB(colours[med])
+    }
+  } else {
+    colour_mean <- colorspace::hex2RGB(colour_mean)
+  }
+  
+  # Set pal to fill in
   pal <- NULL 
+  
+  # keep in mind alpha = b/a+b where b=est_var and a=global_var
   for(i in seq(n)){
     colour <- colorspace::mixcolor(alpha, colour_mean, colours[i])
     pal <- c(pal, colorspace::hex(colour))
   }
   pal
 }
-shrinkage_mix(colourvalues, mean_col, 0.25)
 
 colourvaluesRGB <- colorspace::hex2RGB(colourvalues)
 mean_col <- colorspace::hex(colorspace::mixcolor(0.5, colourvaluesRGB[2], colourvaluesRGB[3]))
@@ -118,8 +132,6 @@ scales::show_col(mean_col)
 scales::show_col(colourvalues,ncol=4)
 scales::show_col(shrinkage_mix(colourvalues, mean_col, 0.5),ncol=4)
 
-perceptual_mix <- function(){
-}
 
 mix_colour <- function(colours, method="tree"){
   # adjust colour based on function
@@ -127,34 +139,47 @@ mix_colour <- function(colours, method="tree"){
     colours <- tree_mix(colours)
   } else if (method == "shrinkage") {
     colours <- shrinkage_mix(colours)
-  } else if (method == "perceptual") {
-    colours <- perceptual_mix(colours)
   } else {
-    stop("The VSUP palette mixing method is not recognised. Please set method to tree, shrinkage, or perceptual.")
+    stop("The VSUP palette mixing method is not recognised. Please set method to tree, shrinkage, or a function of your own.")
   }
   colours
 }
 
-VSUP <- function(colours, method="tree", n=NULL, amount) {
+VSUP <- function(colours, method="tree", type="desaturate", n=NULL,  amount=1) {
+  # set number of mixes in case of odd number
   if(is.null(n)){
     n = 1+ floor(log2(length(colours)))
   } 
+
+  # value can be total change OR vector of increments
+  if (length(amount)==1) {
+    sups <- seq(from = 0, to=amount, length.out = n)[-1]
+  } else if (length(ammount)==n){
+    sups <- amount } else {
+      stop(paste0("The colour transformation amount must be 1 or n (the number of times the colours are mixed. Amount has length ", 
+      length(amount),  
+      "While n is ",
+      n))
+    }
+
+  # Set up matrix that will store colour names
   pal <- matrix(NA, nrow=n, ncol=length(colours))
-  # use a set method
+  pal[1,] <- colours
+  
+  # iteratively transform then mix the colour palette n times
   if(class(method)=="character"){
-    for(i in seq(n)){
+    for(i in seq(n-1)){
+      colours <- transform_colour(colours, type, amount=sups[i])
       colours <- mix_colour(colours, method)
-      pal[i] <- colours
+      pal[i+1,] <- colours
     }
   } else if (class(method)=="function"){
     print("TODO: allow users to suppress with thier own function")
   }
+  pal
 }
-
-VSUP(colourvalues)
-
+scales::show_col(t(VSUP(colourvalues, amount=0.8, method="shrinkage")))
 
 
-mapply(colorspace::mixcolor, 0.5, rgb_colours[odd], rgb_colours[even])
 
 
