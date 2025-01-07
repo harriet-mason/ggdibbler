04fa9db6540002c0bfd6a0234ffc6afa5f9e9b1d
harriet-mason <harriet.mason@monash.edu>
2025-01-07 16:02:24 +1100

    moved map stuff to inst folder

--- inst/VSUP.R
+++ inst/VSUP.R
@@ -180,6 +180,13 @@ VSUP <- function(colours, method="tree", type="desaturate", n=NULL,  amount=1) {
 }
 scales::show_col(t(VSUP(colourvalues, amount=0.8, method="shrinkage")))
 
+# VSUP geom_sf
+geom_sf_VSUP <- function(ggplot, ???){
+  ggplot +
+    geom_sf(aes(fill = ???, geometry = geometry), colour=NA) + 
+    scale_fill_manual(values = VSUP)
+}
+# write up similarly to geom_sf but with VSUP
 
 
 
--- inst/geom_sf_VSUP.R
+++
@@ -1,8 +0,0 @@
-# VSUP geom_sf
-geom_sf_VSUP <- function(ggplot, ???){
-  ggplot +
-    geom_sf(aes(fill = ???, geometry = geometry), colour=NA) + 
-    scale_fill_manual(values = VSUP)
-}
-# write up similarly to geom_sf but with VSUP
-  
