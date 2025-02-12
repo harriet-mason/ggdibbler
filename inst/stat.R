e3f661d2c29c5fe2a40e6c3d083a10bf3d111929
harriet-mason <harriet.mason@monash.edu>
2025-02-12 19:50:30 +1100

    Got stats working! wrote up StatMean, StatMeanVar, and StatSample

--- inst/stat.R
+++ inst/stat.R
@@ -1,36 +1,34 @@
 # working out inputs and outputs
 library(tidyverse)
-library(vctrs)
 devtools::load_all()
 library(distributional)
 
-ggplot() +
-  geom_sf(data=data, aes(geometry, fill=distribution)) +
-  scale_colour_vsup()
+# data with matching names for checking compute_group functions
+named <- toymap |>
+  rename(x = county_name, y = temp_dist) |>
+  select(x, y)
 
-
+# three data sets
 a <- toymap |> select(county_name, temp)
 b <- toymap |> select(county_name, temp_dist)
-
 c <- b |>
   mutate(temp_mean = distributional:::mean.distribution(temp_dist)) %>%
   select(county_name, temp_mean)
 
-ggplot() +
-  geom_point(data=a, aes(x=county_name, y=temp), size=2, colour="blue") +
-  geom_point(data=c, aes(county_name, temp_mean), size=0.5)
+###########################################################################
 
-# ggproto object 
-#make mean/var once working
-StatMean <- ggplot2::ggproto("StatMean", ggplot2::Stat, # stat not found if ggplot2 isn't loaded
+# Location based StatMean
+StatMeanX <- ggplot2::ggproto("StatMean", ggplot2::Stat, 
                        compute_group = function(data, scales) {
-                         mean(data$y)
+                         data$y <- distributional:::mean.distribution(data$y)
+                         data
                        },
                        required_aes = c("y")
 )
 
+
 # Layer function
-stat_mean <- function(mapping = NULL, data = NULL, 
+stat_mean_x <- function(mapping = NULL, data = NULL, 
                                       geom = "point", position = "identity", 
                                       na.rm = FALSE, show.legend = NA, 
                                       inherit.aes = TRUE, ...) {
@@ -45,66 +43,50 @@ stat_mean <- function(mapping = NULL, data = NULL,
     params = list(na.rm = na.rm, ...)
   )
 }
-m <- toymap
 
-# test stat
-ggplot(toymap, aes(x = county_name)) + 
-  geom_point(aes(y=temp), size=2, colour="blue") + 
-  stat_sum
-  stat_mean(aes(y=temp_dist))
 
-######################## TRY WITH COLOUR INSTEAD ####################
-# ggproto object 
-#make mean/var once working
-StatMean <- ggplot2::ggproto("StatMean", Stat, # stat not found if ggplot2 isn't loaded
-                             compute_group = function(data, scales) {
-                               mean(data$colour)
-                               print(mean(data$colour))
-                             },
-                             required_aes = c("colour")
-)
-
-# Layer function
-stat_mean <- function(mapping = NULL, data = NULL, 
-                      geom = "point", position = "identity", 
-                      na.rm = FALSE, show.legend = NA, 
-                      inherit.aes = TRUE, ...) {
-  layer(
-    stat = StatMean, 
-    data = data, 
-    mapping = mapping, 
-    geom = geom, 
-    position = position, 
-    show.legend = show.legend, 
-    inherit.aes = inherit.aes, 
-    params = list(na.rm = na.rm, ...)
-  )
-}
-
-# test stat
-ggplot(toymap, aes(x = county_name, y=temp)) + 
-  geom_point(size=2, colour="black") + 
-  stat_mean(aes(colour=temp_dist))
+# test map
+ggplot() +
+  # Cannot plot dist and normal variables together, bit of a problem
+  # geom_point(data=a, aes(x=county_name, y=temp), size=2, colour="red") +
+  stat_mean_x(data = b, aes(x=county_name, y=temp_dist), size=0.5)
 
 ###########################################################################
 
-StatSample <- ggproto("StatSample", Stat,
-                      compute_group = function(data, scales, n) {
-                        data[distributional::sample(data$x, n), , drop = FALSE]
-                      },
-                      required_aes = c("x")
+# Location based StatSample
+StatSample <- ggplot2::ggproto("StatMeanVar", ggplot2::Stat,
+                               compute_group = function(data, scales, n) {
+                                 data$y <- distributional::generate(data$y, n)
+                                 data
+                               },
+                               required_aes = c("y")
 )
+
+StatSample$compute_group(named, n=10)
+###########################################################################
+
+               
+                                            
+# Fill based StatMeanVar (cant check without propper ggplot)
+StatMeanVar <- ggplot2::ggproto("StatMeanVar", ggplot2::Stat, 
+                              compute_group = function(data, scales) {
+                                data$mfill <- distributional:::mean.distribution(data$fill)
+                                data$vfill <- distributional:::variance.distribution(data$fill)
+                                data
+                              },
+                              required_aes = c("fill")
+)
+# tested compute_group function and it works. Cant check the rest until fill is done
+
+###########################################################################
 
 
 StatPDF <- ggproto()
 
 StatProb <- ggproto()
 
-# Basic DF in DF out approach
-sample_df <- function(df){
 
-
-}
+# try and go between condensed and long data frame
 n=10
 m <- toymap |>
   select(c(county_name, temp_dist, geometry)) |>
