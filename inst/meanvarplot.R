92a3d77eb71b13f6eb61716837b48af7c4d634a8
harriet-mason <harriet.mason@monash.edu>
2025-02-18 23:43:13 +1100

    add toy data stuff to new file

--- inst/meanvarplot.R
+++ inst/meanvarplot.R
@@ -1,4 +1,26 @@
 
+# working out inputs and outputs
+#library(tidyverse)
+#library(distributional)
+devtools::load_all()
+
+toydata <- toymap |>
+  dplyr::filter(county_name == "Story County" | 
+                  county_name == "Boone County" | 
+                  county_name == "Johnson County")
+
+# data with matching names for checking compute_group functions
+named <- toydata |>
+  dplyr::rename(x = county_name, y = temp_dist) |>
+  dplyr::select(x, y)
+
+# three data sets
+a <- toydata |> dplyr::select(county_name, temp)
+b <- toydata |> dplyr::select(county_name, temp_dist)
+c <- b |>
+  dplyr::mutate(temp_mean = distributional:::mean.distribution(temp_dist)) |>
+  dplyr::select(county_name, temp_mean)
+
 # Fill based StatMeanVar (cant check without propper ggplot)
 StatMeanVar <- ggplot2::ggproto("StatMeanVar", ggplot2::Stat, 
                                 compute_group = function(data, scales) {
@@ -9,3 +31,9 @@ StatMeanVar <- ggplot2::ggproto("StatMeanVar", ggplot2::Stat,
                                 required_aes = c("fill")
 )
 # tested compute_group function and it works. Cant check the rest until fill is done
+
+# 
+
+
+# Ideal code
+geom_sf()
