4f415d7d1bd2059ac12ab95a7d3d5e6ce25a6fbb
harriet-mason <harriet.mason@monash.edu>
2025-03-22 01:24:36 +1100

    started fixing toymap data

--- data-raw/toymap.R
+++ data-raw/toymap.R
@@ -24,10 +24,37 @@ toymap <- toymap |>
          count_id = row_number()) 
 
 # make distribution tibble
-toymap <- toymap |> 
-  mutate(temp_dist = dist_normal(temp, se),
-         temp_sample = generate(temp_dist, 10)) |> 
-  select(county_name, geometry, temp_dist, temp_sample)
+# toymap <- toymap |> 
+#  mutate(temp_dist = dist_normal(temp, se),
+#         temp_sample = generate(temp_dist, 50)) |> 
+#  select(county_name, geometry, temp, temp_dist, temp_sample)
+
+
+# "Raw" data
+toymap_raw <- toymap |> 
+  dplyr::reframe(
+    temp_obs = generate(dist_normal(temp, se), 10), 
+    dplyr::across(dplyr::everything())
+  ) |>
+  mutate(longitude = cent_long + runif,
+         )
+
+# Mean
+toymap_mean <- toymap |> 
+  select(county_name, geometry, temp)
+    
+# Sample
+toymap_sample <- toymap |> 
+  select(county_name, geometry, temp_sample) |> 
+  rename(temp = temp_sample)
+
+# Distribution
+toymap_dist <- toymap |> 
+  select(county_name, geometry, temp_dist) |>
+  rename(temp = temp_dist)
+
+
+#toymap_raw
 
 # make into dibble object
 # ??? idk
