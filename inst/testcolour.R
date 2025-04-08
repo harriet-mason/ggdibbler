b2a4985eae9b0810690eba65550df12300dd18d4
harriet-mason <harriet.mason@monash.edu>
2025-04-09 02:01:45 +1000

    trying to work out how vctrs classes work (and mostly making the bivar class)

--- inst/testcolour.R
+++ inst/testcolour.R
@@ -1,6 +1,9 @@
 #### TEST GGCHROMATIC THING
 library(ggdibbler)
-
+library(dplyr)
+library(vctrs)
+library(sf)
+library(distributional)
 ######################### DATA STUFF ########################
 # Mean data
 toy_temp_est <- toy_temp |> 
@@ -14,51 +17,98 @@ ggplot(toy_temp_est) +
               fill=hsl_spec(h=temp_mean, s=temp_se, l=0.5)))
 ################################################################
 
+################### MAKE BIVAR SCALE ########################
 
-################### TEST GGCHROMATIC THING ########################
-library(ggchromatic)
+ggchromatic::cmyk_spec(0.1, 0.2, 0.3, 0)
 
-# Theoretically SHOULD work, does not
-P <- ggplot(mtcars, aes(mpg, disp)) +
-  geom_point(aes(colour = mpg))
-
-rgb_palette(rgb_spec(1, 0, 0))
-
-a <- hcl_spec(10:15, LETTERS[1:6], 12:7)
-a
-class(a)
-b <- hcl_palette(a)
-b
-class(b)
-
-##############################################
-
-hsv_spec <- function(h = double(),
-                     s = double(),
-                     v = double()) {
-  new_colour_spec(h = h, s = s, v = v, class = "hsv_spec")
-}
-
-hcl_spec(1,2,3)
-
-hcl_spec <- function(h = double(),
-                     c = double(),
-                     l = double()) {
-  new_colour_spec(h = h, c = c, l = l, class = "hcl_spec")
-}
-
-new_colour_spec <- function(..., class) {
-  vals <- rlang::list2(1,2,3, "hsv_spec")
+# ggchromatic constructor
+new_not <- function(...) {
+  vals <- rlang::list2(...)
   missing <- vapply(vals, identical, logical(1), quote(expr = ))
   vals[missing] <- list(double())
-  
   n <- lengths(vals)
-  vals[n == 0] <- list(new_void_channel(max(n)))
-  vals <- vctrs::vec_recycle_common(!!!vals)
-  vctrs::new_rcrd(vals, class = c("colour_spec", class))
+  vals <- vec_recycle_common(!!!vals)
+  new_rcrd(vals, class = "dibbler_bivar")
 }
 
-new_void_channel <- function(size = 0) {
-  vctrs::new_vctr(rep(NA, size), class = "void_channel")
+# Make bivar vector class
+new_bivar <- function(...) {
+  #if (!is.double(x)) {
+  #  rlang::abort("`x` must be a double vector.")
+  #}
+  vctrs::new_rcrd(x, class = "dibbler_bivar")
 }
 
+
+# Make bivar stat
+bivar <- function(x=new_dist()) {
+  if (!is_distribution(x)) {
+    rlang::abort("`x` must be a distribution vector.")
+  }
+  est <- sapply(x, distributional:::mean.distribution)
+  se <- sapply(x, distributional::variance)
+  print(mean)
+  print(se)
+  #x <- data.frame(mean = distributional:::mean.distribution(x), 
+  #                se = distributional::variance(x))
+  #x <- lapply(x, extrct_bivar)
+  #x
+  new_bivar(x)
+}
+
+extrct_bivar <- function(x) {
+  # get mean and variance from distribution
+  #x <- c(mean = distributional:::mean.distribution(x), se = distributional::variance(x))
+  print(x)
+  print(length(x))
+  new_bivar(x)
+}
+
+is_bivar <- function(x) {
+  inherits(x, "dibbler_bivar")
+}
+
+format.dibbler_bivar <- function(x, ...) {
+  out <- lapply(vec_data(x), function(x) {
+    return(signif(x, 2))
+  })
+  out <- lapply(out, format)
+  out <- paste0("[", do.call(paste, c(out, sep = ",")), "]")
+  out[is.na(x)] <- NA
+  out
+}
+
+vec_ptype_abbr.dibbler_bivar <- function(x, ...) {
+  "bivar"
+}
+
+vec_ptype2.dibbler_bivar.dibbler_bivar <- function(x, y, ...) new_bivar()
+
+vec_ptype2.dibbler_bivar.double <- function(x, y, ...) double()
+
+vec_ptype2.double.dibbler_bivar <- function(x, y, ...) double()
+
+vec_cast.dibbler_bivar.dibbler_bivar <- function(x, to, ...) x
+
+vec_cast.dibbler_bivar.double <- function(x, to, ...) bivar(x)
+vec_cast.double.dibbler_bivar <- function(x, to, ...) vec_data(x)
+
+vec_cast.dibbler_bivar.distribution <- function(x, to, ...) bivar(x)
+vec_cast.distribution.dibbler_bivar <- function(x, to, ...) dist_normal(x)
+
+
+as_bivar <- function(x) {
+  vec_cast(x, new_bivar())
+}
+
+
+# checking code
+x <- toy_temp_dist$temp_dist
+x <- bivar(x)
+x
+
+vec_cast(x, bivar())
+
+tibble::tibble(x)
+
+vec_ptype2(bivar(), bivar())
