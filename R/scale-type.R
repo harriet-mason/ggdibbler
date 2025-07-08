1108efef63cea9b716999f7b72e9f86453b1f03d
harriet-mason <harriet.mason@monash.edu>
2025-07-09 04:04:40 +1000

    cleaned up documentation of scale_type

--- R/scale-type.R
+++ R/scale-type.R
@@ -1,6 +1,8 @@
 #' Sets default scale for distribution objects
 #' Default scale for distribution objects is continuous
+#'
 #' @param x variable being scaled
+#'
 #' @exportS3Method ggplot2::scale_type
 scale_type.distribution <- function(x) {
   "continuous"
