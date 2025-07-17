5d9d03a297d98b4af9bd90031d58b1a158a56181
harriet-mason <harriet.mason@monash.edu>
2025-07-18 02:07:33 +1000

    update scale-type function to set the correct scale for the distribution output (not just "continuous" lol)

--- R/scale-type.R
+++ R/scale-type.R
@@ -1,9 +1,14 @@
-#' Sets default scale for distribution objects
-#' Default scale for distribution objects is continuous
-#'
+#' ⁠@descriptio Sets default scale for distribution objects
+#' 
+#' ⁠@details Generates a single value from the distribution and uses it to set the default ggplot scale. The scale can be changed later in the ggplot by using any scale_* function
+#' 
+#' @importFrom distributional generate
+#' @importFrom ggplot2 scale_type
 #' @param x variable being scaled
 #'
 #' @exportS3Method ggplot2::scale_type
 scale_type.distribution <- function(x) {
-  "continuous"
+  # generate a single value from a distribution and use it's class to set scale
+  scale_type(unlist(distributional::generate(x,1)))
 }
+
