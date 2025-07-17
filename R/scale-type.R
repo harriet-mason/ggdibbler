9aa4fb97c20733eede145eb9096c1e5f23a301fa
harriet-mason <harriet.mason@monash.edu>
2025-07-18 02:26:27 +1000

    added return to function documentation

--- R/scale-type.R
+++ R/scale-type.R
@@ -1,11 +1,12 @@
-#' ⁠@descriptio Sets default scale for distribution objects
+#' ⁠@description Sets default scale for distribution objects
 #' 
 #' ⁠@details Generates a single value from the distribution and uses it to set the default ggplot scale. The scale can be changed later in the ggplot by using any scale_* function
 #' 
 #' @importFrom distributional generate
 #' @importFrom ggplot2 scale_type
-#' @param x variable being scaled
-#'
+#' @param x value being scaled
+#' @return A character vector of scale types. The scale type is the ggplot scale type of the outcome of the distribution.
+#' 
 #' @exportS3Method ggplot2::scale_type
 scale_type.distribution <- function(x) {
   # generate a single value from a distribution and use it's class to set scale
