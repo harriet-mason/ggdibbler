ea0059fa5f91d11a0a295800db19c0d4dc769890
harriet-mason <harriet.mason@monash.edu>
2025-09-11 17:59:19 +1000

    cleaned up inst stat file for random stats

--- inst/stat.R
+++ inst/stat.R
@@ -1,34 +1,39 @@
-# working out inputs and outputs
-#library(tidyverse)
-#library(distributional)
+###########################################################################
+# General Set up
 devtools::load_all()
 
-toydata <- toymap |>
+toydata <- toy_temp_dist |>
   dplyr::filter(county_name %in% c("Story County", "Boone County", "Johnson County"))
 
-# data with matching names for checking compute_group functions
+# Named dataset for checking compute group
 named <- toydata |>
   dplyr::rename(x = county_name, y = temp_dist) |>
-  dplyr::select(x, y)
+  dplyr::select(x, y) |>
+  sf::st_drop_geometry()
 
-# three data sets
-a <- toydata |> dplyr::select(county_name, temp)
-b <- toydata |> dplyr::select(county_name, temp_dist)
-c <- b |>
-  dplyr::mutate(temp_mean = distributional:::mean.distribution(temp_dist)) |>
-  dplyr::select(county_name, temp_mean)
+# test data set for checking actual data
+test <- toydata |> dplyr::select(county_name, temp_dist)
 
 ###########################################################################
+# StatSampleConditional?
 
-# Location based StatMean
+
+
+
+###########################################################################
+# StatMean
+
 StatMean <- ggplot2::ggproto("StatMean", ggplot2::Stat, 
-                       compute_group = function(data, scales) {
-                         data$y <- distributional:::mean.distribution(data$y)
-                         data
-                       },
-                       required_aes = c("y")
+                             compute_group = function(data, scales) {
+                               data$y <- distributional:::mean.distribution(data$y)
+                               data
+                             },
+                             required_aes = c("y")
 )
 
+# data with matching names for checking compute_group functions
+
+StatMean$compute_group(named)
 
 # Layer function
 stat_mean <- function(mapping = NULL, data = NULL, 
@@ -50,46 +55,12 @@ stat_mean <- function(mapping = NULL, data = NULL,
 
 # test map
 ggplot2::ggplot() +
-  stat_mean(data = b, 
+  stat_mean(data = test, 
             ggplot2::aes(x=county_name, y=temp_dist), 
             size=0.5)
 
 ###########################################################################
-# Location based StatSample
-StatSample <- ggplot2::ggproto("StatSample", ggplot2::Stat,
-                               compute_group = function(data, scales, n) {
-                                 data |>
-                                   dplyr::mutate(y = distributional::generate(y, n)) |>
-                                   tidyr::unnest(y)
-                                 
-                               },
-                               required_aes = c("y")
-)
-
-stat_sample <- function(mapping = NULL, data = NULL, 
-                        geom = "point", position = "identity", 
-                        na.rm = FALSE, show.legend = NA, 
-                        inherit.aes = TRUE, n=10, ...) {
-  ggplot2::layer(
-    stat = StatSample, 
-    data = data, 
-    mapping = mapping, 
-    geom = geom, 
-    position = position, 
-    show.legend = show.legend, 
-    inherit.aes = inherit.aes, 
-    params = list(na.rm = na.rm,
-                  n = n, ...)
-  )
-}
-
-# test plot
-ggplot2::ggplot() +
-  stat_sample(data = b, 
-              ggplot2::aes(x=county_name, y=temp_dist), n=30,
-              size=0.5)
-
-###########################################################################
+# Stat Prob
 
 StatProb <- ggplot2::ggproto("StatProb", ggplot2::Stat,
                     compute_group = function(data, scales, p = NULL) {
