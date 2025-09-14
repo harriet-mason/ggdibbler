79b4eaa929dc5438f1d67c13ce9d47a011261dcf
harriet-mason <harriet.mason@monash.edu>
2025-09-14 20:46:07 +1000

    just logged more of my suffering

--- inst/point-sample-error.R
+++ inst/point-sample-error.R
@@ -18,31 +18,11 @@ test_data <- data.frame(
 )
 
 # position case
-p <- ggplot() + 
-  stat_sample(data = test_data, ggplot2::aes(x=bob, y=john))
-  
+plot <- ggplot() + 
+  stat_sample(data = test_data, ggplot2::aes(x=bob, y=john)) +
+  scale_x_continuous(trans="dist") +
+  scale_y_continuous(trans="dist")
 
-# Colour case
-basic_data <- toy_temp_dist |>
-  dplyr::filter(county_name %in% c("Pottawattamie County", "Mills County", "Cass County"))
-
-p2 <- basic_data |>
-  ggplot() + 
-  geom_sf_sample(aes(geometry = county_geometry, fill=temp_dist)) +
-  scale_fill_distiller(palette = "OrRd")
-
-# This returns scale error
-# ggplot_build(p)
-
-
-# Internals of ggplot_build(p) AKA build_ggplot(p)
-plot <- p
-# idk what the point of this is
-plot <- ggplot2:::plot_clone(plot)
-# check I guess it checks if you passed empty ggplot()
-if (length(plot@layers) == 0) {
-  plot <- plot + geom_blank()
-}
 # set layers, data, and scales
 layers <- plot@layers
 data <- rep(list(NULL), length(layers))
@@ -69,8 +49,85 @@ data <- lapply(data, scales$transform_df)
 scale_x <- function() scales$get_scales("x")
 scale_y <- function() scales$get_scales("y")
 
-# This throws error
+
 layout$train_position(data, scale_x(), scale_y())
+data <- layout$map_position(data)
+data <- .expose_data(data)
+
+# Apply and map statistics
+data <- ggplot2:::by_layer(function(l, d) l$compute_statistic(d, layout), layers, data, "computing stat")
+data <- ggplot2:::by_layer(function(l, d) l$map_statistic(d, plot), layers, data, "mapping stat to aesthetics")
+
+# Make sure missing (but required) aesthetics are added
+plot@scales$add_missing(c("x", "y"), plot@plot_env)
+
+# Reparameterise geoms from (e.g.) y and width to ymin and ymax
+data <- by_layer(function(l, d) l$compute_geom_1(d), layers, data, "setting up geom")
+
+# Apply position adjustments
+data <- by_layer(function(l, d) l$compute_position(d, layout), layers, data, "computing position")
+
+# Reset position scales, then re-train and map.  This ensures that facets
+# have control over the range of a plot: is it generated from what is
+# displayed, or does it include the range of underlying data
+data <- .ignore_data(data)
+layout$reset_scales()
+layout$train_position(data, scale_x(), scale_y())
+layout$setup_panel_params()
+data <- layout$map_position(data)
+
+# Hand off position guides to layout
+layout$setup_panel_guides(plot@guides, plot@layers)
+
+# Complete the plot's theme
+plot@theme <- plot_theme(plot)
+
 
 ####################################################################################
+
+dist <- dist_normal(mu = 1:5, sigma = 3)
+t <- transform_dist()
+t$transform(dist)
+t$inverse(t$transform(dist))
+
+####################################################################################
+# Check scale applied to other ggplots
+# make data that doesn't work
+
+# Distribution case
+plot <- ggplot() + 
+  stat_sample(data = test_data, ggplot2::aes(x=bob, y=john))
+scales <- plot@scales
+data <- lapply(data, scales$transform_df)
+scales$transform_df
+
+####################################################################################
+# continusous data case
+plot <- ggplot(mpg, aes(displ, hwy, colour = class)) +
+  geom_point(stat = "summary") +
+  scale_x_log10()
+
+
+# set layers, data, and scales
+layers <- plot@layers
+data <- rep(list(NULL), length(layers))
+scales <- plot@scales
+
+# something about final adjustments??
+data <- ggplot2:::by_layer(function(l, d) l$layer_data(plot@data), layers, data, "computing layer data")
+data <- ggplot2:::by_layer(function(l, d) l$setup_layer(d, plot), layers, data, "setting up layer")
+
+# initialise panels
+layout <- ggplot2:::create_layout(plot@facet, plot@coordinates, plot@layout)
+data <- layout$setup(data, plot@data, plot@plot_env)
+
+# Compute aesthetics to produce data with generalised variable names
+data <- ggplot2:::by_layer(function(l, d) l$compute_aesthetics(d, plot), layers, data, "computing aesthetics")
+plot@labels <- ggplot2:::setup_plot_labels(plot, layers, data)
+data <- .ignore_data(data)
+
+# Transform all scales
+data <- lapply(data, scales$transform_df)
+
+
 
