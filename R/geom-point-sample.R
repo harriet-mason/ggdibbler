fa5eebc7c13619a0c06b2bb5a825bc5f461775d6
harriet-mason <harriet.mason@monash.edu>
2025-10-20 21:21:03 +1100

    updateed geom_point documentation

--- R/geom-point-sample.R
+++ R/geom-point-sample.R
@@ -15,12 +15,12 @@
 #' library(distributional)
 #' set.seed(1997)
 #' point_data <- data.frame(
-#'   random_x = c(distributional::dist_uniform(2,3),
-#'                distributional::dist_normal(3,2), 
-#'              distributional::dist_exponential(3)),
-#'   random_y = c(distributional::dist_gamma(2,1),
+#'   random_x = c(dist_uniform(2,3),
+#'                dist_normal(3,2), 
+#'              dist_exponential(3)),
+#'   random_y = c(dist_gamma(2,1),
 #'              dist_sample(x = list(rnorm(100, 5, 1))),
-#'              distributional::dist_exponential(1)),
+#'              dist_exponential(1)),
 #'    # have some uncertainty as to which category each value belongs to
 #'   random_colour = dist_categorical(prob = list(c(0.8,0.15,0.05),
 #'                                                  c(0.25,0.7,0.05),
@@ -34,20 +34,20 @@
 #'   
 #' # basic random variables x and y
 #' ggplot() + 
-#'   geom_point_sample(data = point_data, ggplot2::aes(x=random_x, y=random_y))
+#'   geom_point_sample(data = point_data, aes(x=random_x, y=random_y))
 #'
 #' # random variables only x
 #' ggplot() + 
-#'   geom_point_sample(data = point_data, ggplot2::aes(x=random_x, y=deterministic_xy), 
+#'   geom_point_sample(data = point_data, aes(x=random_x, y=deterministic_xy), 
 #'                     alpha = 0.3, times=50)
 #'   
 #' # deterministic colour, random x and y
 #' ggplot() + 
-#'   geom_point_sample(data = point_data, ggplot2::aes(x=random_x, y=random_y, colour = deterministic_colour))
+#'   geom_point_sample(data = point_data, aes(x=random_x, y=random_y, colour = deterministic_colour))
 #'   
 #' # random x, y, and colour
 #' ggplot() + 
-#'   geom_point_sample(data = point_data, ggplot2::aes(x=random_x, y=random_y, colour = random_colour), 
+#'   geom_point_sample(data = point_data, aes(x=random_x, y=random_y, colour = random_colour), 
 #'                     times=1000, alpha=0.3)
 #' @export
 geom_point_sample <- function(mapping = NULL, data = NULL, stat = "identity", position = "identity",
