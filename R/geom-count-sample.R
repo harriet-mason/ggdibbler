624047885a537e480ac8525de129e2f37c890ef7
harriet-mason <harriet.mason@monash.edu>
2025-10-25 01:47:13 +1100

    added example to geom_count_sample

--- R/geom-count-sample.R
+++ R/geom-count-sample.R
@@ -8,6 +8,31 @@
 #' @param times A parameter used to control the number of values sampled from each distribution.
 #' @examples
 #' library(ggplot2)
-#' library(distributional)
+#' 
+#' # Discrete values have overplotting
+#' # ggplot
+#' ggplot(mpg, aes(cty, hwy)) +
+#'   geom_point()
+#' # ggdibbler
+#' ggplot(uncertain_mpg, aes(cty, hwy)) +
+#'   geom_point_sample()
+#'
+#'# Can use geom_count to fix it
+#' # ggplot
+#' ggplot(mpg, aes(cty, hwy)) +
+#'   geom_count()
+#' # ggdibbler (alpha for resample overlap)
+#' ggplot(uncertain_mpg, aes(cty, hwy)) +
+#'   geom_count_sample(alpha=0.2) 
+#'   
+#' # Best used in conjunction with scale_size_area 
+#' # ggplot
+#' ggplot(mpg, aes(cty, hwy)) +
+#'   geom_count() +
+#'   scale_size_area()
+#' # ggdibbler
+#' ggplot(uncertain_mpg, aes(cty, hwy)) +
+#'   geom_count_sample(alpha=0.2) +
+#'   scale_size_area()
 #' @export
 geom_count_sample <- make_constructor(GeomPoint, stat = "sum_sample", times=10)
