cdf55f201dcbfe8921360b57edde8fb348bb2724
harriet-mason <harriet.mason@monash.edu>
2025-10-24 13:55:04 +1100

    fixed stat_sum documentation up

--- R/stat-sum-sample.R
+++ R/stat-sum-sample.R
@@ -1,6 +1,5 @@
-
-#' @importFrom ggplot2 ggproto StatSum self
-#' @rdname Stat
+#' @importFrom ggplot2 ggproto StatSum
+#' @rdname geom_count_sample
 #' @format NULL
 #' @usage NULL
 #' @export
@@ -76,7 +75,7 @@ StatSumSample <- ggplot2::ggproto("StatSumSample", ggplot2::StatSum,
 )
 
 #' @export
-#' @rdname geom_count
+#' @rdname geom_count_sample
 #' @inheritParams ggplot2::stat_sum
 #' @param times A parameter used to control the number of values sampled from each distribution. By default, times is set to 30.
 stat_sum_sample <- make_constructor(StatSumSample, geom = "point", times = 30)
