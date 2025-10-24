4e22e2c84cda50cae46fe1a3dd8aa017b2b7d8b4
harriet-mason <harriet.mason@monash.edu>
2025-10-24 13:54:43 +1100

    updated example for geom_point_sample

--- R/geom-point-sample.R
+++ R/geom-point-sample.R
@@ -10,43 +10,48 @@
 #' @inheritParams ggplot2::geom_point
 #' @examples
 #' library(ggplot2)
-#' library(distributional)
-#' set.seed(1997)
-#' point_data <- data.frame(
-#'   random_x = c(dist_uniform(2,3),
-#'                dist_normal(3,2), 
-#'              dist_exponential(3)),
-#'   random_y = c(dist_gamma(2,1),
-#'              dist_sample(x = list(rnorm(100, 5, 1))),
-#'              dist_exponential(1)),
-#'    # have some uncertainty as to which category each value belongs to
-#'   random_colour = dist_categorical(prob = list(c(0.8,0.15,0.05),
-#'                                                  c(0.25,0.7,0.05),
-#'                                                  c(0.25,0,0.75)), 
-#'                                      outcomes = list(c("A", "B", "C"))),
-#'   deterministic_xy = c(1,2,3),
-#'   deterministic_colour = c("A", "B", "C"))
+#' 
+#'   # ggplot
+#' p <- ggplot(mtcars, aes(wt, mpg))
+#' p + geom_point()
+#' 
+#'   # ggdibbler
+#' q <- ggplot(uncertain_mtcars, aes(wt, mpg))
+#' q + geom_point_sample()
+#' 
+#' # Add aesthetic mappings
+#' 
+#'  # ggplot
+#' p + geom_point(aes(colour = factor(cyl)))
+#'   # ggdibbler - a
+#' q + geom_point_sample(aes(colour = cyl))
+#'   # ggdibbler - b
+#'   # If you want the categorical colour for a factor,
+#'   # You would need to have a categorical rand variable, do compute with after_stat
+#' ggplot(data = uncertain_mtcars, aes(x=wt, y=mpg, distcol=cyl)) + 
+#'   geom_point_sample(aes(colour = factor(after_stat(distcol))))
 #'   
-#' # basic random variables x and y
-#' ggplot() + 
-#'   geom_point_sample(data = point_data, aes(x=random_x, y=random_y))
-#'
-#' # random variables only x
-#' ggplot() + 
-#'   geom_point_sample(data = point_data, aes(x=random_x, y=deterministic_xy), 
-#'                     alpha = 0.3, times=50)
-#'   
-#' # deterministic colour, random x and y
-#' ggplot() + 
-#'   geom_point_sample(data = point_data, aes(x=random_x, y=random_y, colour = deterministic_colour))
-#'   
-#' # random x, y, and colour
-#' ggplot() + 
-#'   geom_point_sample(data = point_data, aes(x=random_x, y=random_y, colour = random_colour), 
-#'                     times=1000, alpha=0.3)
+#'  # ggplot
+#' p + geom_point(aes(shape = factor(cyl))) 
+#'   # ggdibbler - a
+#' q + geom_point_sample(aes(shape = cyl)) + scale_shape_binned()
+#'   # ggdibbler - b
+#' ggplot(data = uncertain_mtcars, aes(x=wt, y=mpg, distshape=cyl)) +
+#'   geom_point_sample(aes(shape = factor(after_stat(distshape))))
+#'  
+#' # A "bubblechart":
+#' # ggplot2
+#' p + geom_point(aes(size = qsec))
+#' # ggdibbler
+#' q + geom_point_sample(aes(size = qsec), alpha=0.5, times=5)
+#' 
+#' Set aesthetics to fixed value
+#' # ggplot
+#' ggplot(mtcars, aes(wt, mpg)) + geom_point(colour = "red", size = 3)
+#' # ggdibbler
+#' ggplot(uncertain_mtcars, aes(wt, mpg)) + geom_point_sample(colour = "red", size = 3)
 #' @export
 geom_point_sample <- make_constructor(GeomPoint, stat = "sample", times=30)
-
 
 
 
