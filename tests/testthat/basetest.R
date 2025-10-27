521410fbe5b2b8caf287e2b3821bbb2ebfc78c9b
harriet-mason <harriet.mason@monash.edu>
2025-10-28 01:06:54 +1100

    added file to hold generic test format

---
+++ tests/testthat/basetest.R
@@ -0,0 +1,18 @@
+
+####################### PASS #################
+library(vdiffr)
+
+test_that("geom_**_sample tests", {
+  # no random variables used - just return normal points
+  set.seed(***)
+  p1 <- 
+    expect_doppelganger("example1", p1)
+  
+  # random variables x and y
+  p2 <- 
+    expect_doppelganger("example2", p2)
+}
+)
+
+####################### FAIL #################
+####################### UNTESTED #################
