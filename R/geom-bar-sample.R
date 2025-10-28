afce38d165e9e4a80e2e6ef4af0c5599680da87c
harriet-mason <harriet.mason@monash.edu>
2025-10-28 17:39:49 +1100

    added geom_bar sample

---
+++ R/geom-bar-sample.R
@@ -0,0 +1,38 @@
+#' Uncertain Bar Charts
+#' 
+#' Identical to geom_bar, except that it will accept a distribution in place of any of the usual aesthetics.
+#' 
+#' @inheritParams ggplot2::geom_bar
+#' @importFrom ggplot2 make_constructor GeomBar
+#' @param times A parameter used to control the number of values sampled from each distribution.
+#' @examples
+#' g <- ggplot(mpg, aes(class)) #ggplot
+#' q <- ggplot(uncertain_mpg, aes(class)) #ggdibbler
+#' 
+#' # Number of cars in each class:
+#' g + geom_bar() #ggplot
+#' q + geom_bar_sample() #ggdibbler
+#' 
+#' # Total engine displacement of each class
+#' g + geom_bar(aes(weight = displ)) #ggplot
+#' q + geom_bar_sample(aes(weight = displ)) #ggdibbler
+#' 
+#' # Map class to y instead to flip the orientation
+#' ggplot(mpg) + geom_bar(aes(y = class)) #ggplot
+#' ggplot(uncertain_mpg) + geom_bar_sample(aes(y = class)) #ggdibbler
+#' 
+#' # geom_col also has a sample counterpart
+#' df <- data.frame(trt = c("a", "b", "c"), outcome = c(2.3, 1.9, 3.2))
+#' uncertain_df <-  data.frame(trt = c("a", "b", "c"), 
+#'                             outcome = dist_normal(mean = c(2.3, 1.9, 3.2), sd = c(0.5, 0.8, 0.7)))
+#' # ggplot
+#' ggplot(df, aes(trt, outcome)) +
+#'   geom_col()
+#' # ggdibbler
+#' ggplot(uncertain_df, aes(x=trt, y=outcome)) +
+#'   geom_col_sample(alpha=0.05, position = "identity", times=30)
+#' ggplot(uncertain_df, aes(x=trt, y=outcome)) +
+#'   geom_col_sample(times = 30)
+#' @export
+geom_bar_sample <- make_constructor(ggplot2::GeomBar, stat = "count_sample", position="dodge",
+                                    just = 0.5, times=10)
---
+++ R/stat-count-sample.R
@@ -0,0 +1,55 @@
+#' @importFrom ggplot2 ggproto StatCount
+#' @rdname geom_count_sample
+#' @format NULL
+#' @usage NULL
+#' @export
+StatCountSample <- ggplot2::ggproto("StatCountSample", ggplot2::StatCount,
+                                    setup_params = function(self, data, params) {
+                                      params$flipped_aes <- has_flipped_aes(data, params, main_is_orthogonal = FALSE)
+                                      
+                                      has_x <- !(is.null(data$x) && is.null(params$x))
+                                      has_y <- !(is.null(data$y) && is.null(params$y))
+                                      if (!has_x && !has_y) {
+                                        cli::cli_abort("{.fn {snake_class(self)}} requires an {.field x} or {.field y} aesthetic.")
+                                      }
+                                      if (has_x && has_y) {
+                                        cli::cli_abort("{.fn {snake_class(self)}} must only have an {.field x} {.emph or} {.field y} aesthetic.")
+                                      }
+                                      if (is.null(params$width)) {
+                                        x <- if (params$flipped_aes) "y" else "x"
+                                        params$width <- resolution_dist(data[[x]], discrete = TRUE) * 0.9
+                                      }
+                                      # print(params$width)
+                                      
+                                      params
+                                    },
+                                  
+                                  
+                                  setup_data = function(data, params) {
+                                    sample_expand(data, params$times) 
+                                  },
+                                  
+                                  extra_params = c("na.rm", "times", "orientation",
+                                                   "width", "flipped_aes"),
+                                  
+                                  
+)
+
+#' @export
+#' @rdname geom_count_sample
+#' @importFrom ggplot2 make_constructor StatCount
+#' @inheritParams ggplot2::stat_count
+#' @param times A parameter used to control the number of values sampled from each distribution. By default, times is set to 30.
+stat_count <- make_constructor(
+  ggplot2::StatCount, geom = "bar", # position = "stack",
+  orientation = NA, omit = "width", times=10,
+)
+
+
+#' @keywords internal
+resolution_dist <- function(x, zero = TRUE, discrete = FALSE) {
+  if(distributional::is_distribution(x)){
+    x <- unlist(generate(x, 10))
+  }
+  resolution(x)
+}
