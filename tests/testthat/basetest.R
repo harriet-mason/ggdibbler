99ca79182eb3d79f1848719e3fce321d1984ecae
harriet-mason <harriet.mason@monash.edu>
2025-10-28 17:38:55 +1100

    added tests and commented out code I know doesn't work

--- tests/testthat/basetest.R
+++ tests/testthat/basetest.R
@@ -1,18 +1,18 @@
 
-####################### PASS #################
-library(vdiffr)
-
-test_that("geom_**_sample tests", {
-  # no random variables used - just return normal points
-  set.seed(***)
-  p1 <- 
-    expect_doppelganger("example1", p1)
-  
-  # random variables x and y
-  p2 <- 
-    expect_doppelganger("example2", p2)
-}
-)
-
-####################### FAIL #################
-####################### UNTESTED #################
+# library(vdiffr)
+# 
+# test_that("geom_**_sample tests", {
+#   # no random variables used - just return normal points
+#   set.seed(***)
+#   p1 <- 
+#     expect_doppelganger("example1", p1)
+#   
+#   # random variables x and y
+#   p2 <- 
+#     expect_doppelganger("example2", p2)
+# }
+# )
+# 
+# ####################### PASS #################
+# ####################### FAIL #################
+# ####################### UNTESTED #################
