52afc01c1b7858b4adf06751fb6932151b31e31d
harriet-mason <harriet.mason@monash.edu>
2025-11-03 00:20:58 +1100

    split up geom_tile_sample functions

---
+++ R/geom-raster-sample.R
@@ -0,0 +1,11 @@
+#' @inheritParams ggplot2::geom_raster
+#' @importFrom ggplot2 make_constructor GeomRaster
+#' @param times A parameter used to control the number of values sampled from each distribution.
+#' @export
+#' @rdname geom_tile_sample
+geom_raster_sample <- make_constructor(
+  ggplot2::GeomRaster, stat = "identity_sample", times=10,
+  checks = exprs(
+    check_number_decimal(hjust), 
+    check_number_decimal(vjust))
+)
--- R/geom_tile_sample.R
+++ R/geom_tile_sample.R
@@ -10,25 +10,3 @@
 #' @export
 geom_tile_sample <- make_constructor(ggplot2::GeomTile, stat = "identity_sample", times=10,
                                      position = "identity_subdivide")
-
-
-#' @inheritParams ggplot2::geom_rect
-#' @importFrom ggplot2 make_constructor GeomRect
-#' @param times A parameter used to control the number of values sampled from each distribution.
-#' @export
-#' @rdname geom_tile_sample
-geom_rect_sample <- make_constructor(ggplot2::GeomRect, stat = "identity_sample", times=10,
-                                     position = "identity_subdivide")
-
-
-#' @inheritParams ggplot2::geom_raster
-#' @importFrom ggplot2 make_constructor GeomRaster
-#' @param times A parameter used to control the number of values sampled from each distribution.
-#' @export
-#' @rdname geom_tile_sample
-geom_raster_sample <- make_constructor(
-  ggplot2::GeomRaster, stat = "identity_sample", times=10,
-  checks = exprs(
-    check_number_decimal(hjust), 
-    check_number_decimal(vjust))
-  )
