31b1d52c233090a315c32ca15b0894537b14b16d
harriet-mason <harriet.mason@monash.edu>
2025-11-06 23:25:32 +1100

    removed warning test

--- tests/testthat/test-geom-quantile-sample.R
+++ tests/testthat/test-geom-quantile-sample.R
@@ -1,19 +1,18 @@
 
 library(ggplot2)
 library(vdiffr)
+n <- ggplot(uncertain_mpg, aes(displ, hwy))
 
 test_that("geom_quantile_sample tests", {
   
   set.seed(4343)
   
-  n <- ggplot(uncertain_mpg, aes(displ, hwy)) 
-  
   p1 <- n + geom_quantile_sample(alpha=0.5, linewidth=0.5)
   expect_doppelganger("Example 1", p1)
   
-  q10 <- seq(0.05, 0.95, by = 0.05)
-  p2 <- n + geom_quantile_sample(quantiles = q10, alpha=0.5, linewidth=0.5)
-  expect_doppelganger("Example 2", p2)
+  # q10 <- seq(0.05, 0.95, by = 0.05)
+  # p2 <- n + geom_quantile_sample(quantiles = q10, alpha=0.5, linewidth=0.5)
+  # expect_doppelganger("Example 2", p2)
   
   p3 <- n + geom_quantile_sample(method = "rqss",
                                  alpha=0.5, linewidth=0.5)
