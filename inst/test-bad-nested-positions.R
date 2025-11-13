0f4a60132e152e24ca892e3098549e6cff92cc9e
harriet-mason <harriet.mason@monash.edu>
2025-11-13 19:26:45 +1100

    finished playing with stacked identity

--- inst/test-bad-nested-positions.R
+++ inst/test-bad-nested-positions.R
@@ -31,27 +31,26 @@ ggplot(uncertain_mpg, aes(class)) +
   geom_bar_sample(aes(fill = drv), position = "dodge_stack")
 
 
+
 # MAIN IS STACK
 g + geom_bar(aes(fill = drv), position="stack")
 
-set.seed(3)
-ggplot(uncertain_mpg, aes(class)) + 
-  geom_bar_sample(aes(fill = drv), position = "stack")
+
 
 set.seed(3)
 ggplot(uncertain_mpg, aes(class)) + 
-  geom_bar_sample(aes(fill = drv), position = "stack_identity", alpha=0.1)
+  geom_bar_sample(aes(fill = drv), position = "identity_stack", alpha=0.7)
 
 set.seed(3)
 ggplot(uncertain_mpg, aes(class)) + 
-  geom_bar_sample(aes(fill = drv), position = "stack_identity", alpha=0.05, times=30)
+  geom_bar_sample(aes(fill = drv), position = "stack_identity", alpha=0.7)
 
 set.seed(3)
 ggplot(uncertain_mpg, aes(class)) + 
-   geom_bar_sample(aes(fill = drv), position = "stack_stack1", alpha=0.1, times=30)
-set.seed(3)
-ggplot(uncertain_mpg, aes(class)) + 
-  geom_bar_sample(aes(fill = drv), position = "stack_stack2", alpha=0.1, times=30)
+  geom_bar_sample(aes(fill = drv), position = "stack", times=2)
+
+
+
 
 # check when functions not printing
 # set.seed(3)
