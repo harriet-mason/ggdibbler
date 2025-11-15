fab94b85df3eef8e6e7fdf1a478bae59e9b20cc9
harriet-mason <harriet.mason@monash.edu>
2025-11-15 20:07:52 +1100

    Cleaned up tests after position stuff

--- tests/testthat/test-nest-position.R
+++
@@ -1,6 +0,0 @@
-library(ggplot2)
-position_by_group(mpg, "model")
-
-
-mpg_test <- sample_expand(uncertain_mpg, 5)
-position_by_group(mpg_test, "drawID", PositionDodgeIdentity$compute_group)
--- tests/testthat/test-position-nesting-utils.R
+++ tests/testthat/test-position-nesting-utils.R
@@ -1,34 +1,17 @@
 
-set.seed(3)
-ggplot(uncertain_mpg, aes(class)) + 
-  geom_bar_sample(aes(fill = drv), position = "stack_dodge")
-set.seed(3)
-ggplot(uncertain_mpg, aes(class)) + 
-  geom_bar_sample(aes(fill = drv), position = position_nest("stack_dodge"))
-
-set.seed(3)
-ggplot(uncertain_mpg, aes(class)) + 
-  geom_bar_sample(aes(fill = drv), position = "dodge_dodge")
-set.seed(3)
-ggplot(uncertain_mpg, aes(class)) + 
-  geom_bar_sample(aes(fill = drv), position = position_nest("dodge_dodge"))
-
-ggplot(mtcars, aes(am, vs)) +
-  geom_point()
-set.seed(3)
-ggplot(mtcars, aes(am, vs)) +
-  geom_point(position = "jitter")
-
-
-set.seed(3)
-ggplot(uncertain_mtcars, aes(am, vs)) +
-  geom_point_sample()
-generate(uncertain_mtcars$am,times=20)
-generate(uncertain_mtcars$am,times=20)uncertain_mtcars$vs
-ggplot(mtcars, aes(am, vs)) +
-  geom_jitter(position = position_jitter(width = 0.1, height = 0.1))
-
-
-
+test_that("geom_tile_sample tests", {
+  set.seed(443)
+  
+  p1 <- ggplot(uncertain_mpg, aes(class)) + 
+    geom_bar_sample(aes(fill = drv), 
+                    position = position_nest("stack_dodge"))
+  expect_doppelganger("Example 1", p1)
+  
+  p2 <- ggplot(uncertain_mpg, aes(class)) + 
+    geom_bar_sample(aes(fill = drv), 
+                    position = position_nest("dodge_dodge"))
+  expect_doppelganger("Example 2", p2)
+}
+)
 
 
