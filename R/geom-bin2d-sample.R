840174e02b79612300ab267e6e2462b9fd11177e
harriet-mason <harriet.mason@monash.edu>
2025-11-16 22:21:02 +1100

    finished geom_hex

--- R/geom-bin2d-sample.R
+++ R/geom-bin2d-sample.R
@@ -6,7 +6,18 @@
 #' @importFrom ggplot2 make_constructor GeomBin2d
 #' @param times A parameter used to control the number of values sampled from each distribution.
 #' @examples
-#' print("replace me")
+#' # ggplot
+#' d <- ggplot(smaller_diamonds, aes(x, y)) 
+#' d + geom_bin_2d()
+#' # ggdibbler
+#' b <- ggplot(smaller_uncertain_diamonds, aes(x, y)) 
+#' # the ggdibbler default position adjustment is dodging
+#' b + geom_bin_2d_sample(times=100)
+#' # but it can change it to be transparency
+#' b + geom_bin_2d_sample(position="identity", alpha=0.2)
+#' # Still have the same options
+#' d + geom_bin_2d(bins = 10) #ggplot
+#' b + geom_bin_2d_sample(bins = 10) #ggdibbler
 #' @export
 geom_bin_2d_sample <- make_constructor(ggplot2::GeomBin2d, stat = "bin2d_sample", 
                                        times=10, position="identity_dodge")
--- tests/testthat/test-geom-bin2d-sample.R
+++ tests/testthat/test-geom-bin2d-sample.R
@@ -1,33 +1,25 @@
 library(vdiffr)
+library(ggplot2)
+b <- ggplot(smaller_uncertain_diamonds, aes(x, y)) 
 
-test_that("geom_**_sample tests", {
+test_that("geom_bin_2d_sample tests", {
   
-  set.seed(***)
+  set.seed(999)
   
+  p1 <- b + geom_bin_2d_sample(times=100)
   expect_doppelganger("Example 1", p1)
+  
+  p2 <- b + geom_bin_2d_sample(position="identity", alpha=0.2)
+  expect_doppelganger("Example 2", p2)
+  
+  p3 <- b + geom_bin_2d_sample(bins = 10) #ggdibbler
+  expect_doppelganger("Example 3", p3)
+  
+  p4 <- b + geom_bin_2d_sample(bins = list(x = 30, y = 10)) #ggdibbler
+  expect_doppelganger("Example 4", p4)
+  
+  p5 <- b + geom_bin_2d_sample(binwidth = c(0.1, 0.1))
+  expect_doppelganger("Example 5", p5)
   
 }
 )
-
-################ PASS #################
-############### FAIL #################
-############## UNTESTED #################
-# ggplot
-d <- ggplot(smaller_diamonds, aes(x, y)) 
-d + geom_bin_2d()
-# ggdibbler
-b <- ggplot(smaller_uncertain_diamonds, aes(x, y)) 
-  # the ggdibbler default position adjustment is dodging
-b + geom_bin_2d_sample(times=100)
-  # but it can change it to be transparency
-b + geom_bin_2d_sample(position="identity", alpha=0.2)
-
-# You can control the size of the bins by specifying the number of
-# bins in each direction:
-d + geom_bin_2d(bins = 10) #ggplot
-b + geom_bin_2d_sample(bins = 10) #ggdibbler
-d + geom_bin_2d(bins = list(x = 30, y = 10))
-b + geom_bin_2d_sample(bins = list(x = 30, y = 10))
-# Or by specifying the width of the bins
-d + geom_bin_2d(binwidth = c(0.1, 0.1))
-b + geom_bin_2d_sample(binwidth = c(0.1, 0.1))
