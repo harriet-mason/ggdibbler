89c5bde6fb27a1e1595e8e767fd292f8276b1ce6
harriet-mason <harriet.mason@monash.edu>
2025-11-24 00:49:37 +1100

    Finished stat-summary-bin-sample

---
+++ R/stat-summary-bin-sample.R
@@ -0,0 +1,33 @@
+#' @importFrom ggplot2 ggproto StatSummaryBin
+#' @rdname stat_summary_sample
+#' @format NULL
+#' @usage NULL
+#' @export
+StatSummaryBinSample <- ggplot2::ggproto("StatSummaryBinSample", ggplot2::StatSummaryBin,
+                                         setup_params = function(self, data, params) {
+                                           times <- params$times
+                                           params$times <- 1
+                                           data <- dibble_to_tibble(data, params)
+                                           params <- ggplot2::ggproto_parent(ggplot2::StatSummaryBin, 
+                                                                             self)$setup_params(data, params)
+                                           params$times <- times
+                                           params
+                                         },
+                                         
+                                        setup_data = function(data, params) {
+                                          dibble_to_tibble(data, params) 
+                                        },
+                                        
+                                        extra_params = c("na.rm", "times", "fun.data", "bins", 
+                                                         "fun", "fun.min", "fun.max", "binwidth", "breaks",
+                                                         "fun.args", "orientation", "show.legend", "inherit.aes")
+)
+
+#' @rdname stat_summary_sample
+#' @inheritParams ggplot2::stat_summary_bin
+#' @importFrom ggplot2 make_constructor
+#' @export
+stat_summary_bin_sample <- make_constructor(StatSummaryBinSample, 
+                                            geom = "pointrange",
+                                            times = 10, alpha=2/times)
+
---
+++ tests/testthat/test-stat-summary-bin-sample.R
@@ -0,0 +1,15 @@
+
+suppressWarnings({
+library(vdiffr)
+library(ggplot2)
+  test_that("stat_summary_bin_sample tests", {
+    
+    set.seed(123)
+    p1 <- ggplot(smaller_uncertain_diamonds, aes(carat, price)) +
+      stat_summary_bin_sample(fun = "mean", geom = "bar", orientation = 'y')
+    expect_doppelganger("Example 1", p1)
+    }
+  )
+})
+
+
